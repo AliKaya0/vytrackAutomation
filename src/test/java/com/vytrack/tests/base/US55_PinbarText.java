25fee67d3b1311a3536ade5fb1c92e9d1862d266
Alex2055 <alexfromnashville@gmail.com>
2022-03-11 12:51:41 -0600

    Sprint2_US55 two test cases for pinbar created

---
+++ src/test/java/com/vytrack/tests/base/US55_PinbarText.java
@@ -0,0 +1,39 @@
+package com.vytrack.tests.base;
+
+import com.vytrack.utilities.Driver;
+import com.vytrack.utilities.VytrackUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class US55_PinbarText {
+
+    @Test
+
+    public void pinBarImageTest() {
+
+        VytrackUtils.loginAsDriver();
+
+        WebElement pinbarLink = Driver.getDriver().findElement(By.xpath("(//a[@title=\"How To Use Pinbar\"])[1]"));
+
+        pinbarLink.click();
+
+        WebElement message1 = Driver.getDriver().findElement(By.xpath("//div[@class='clearfix']//h3"));
+
+        String actualMessage1 = message1.getText();
+        String expectedMessage1 = "How To Use Pinbar";
+
+        Assert.assertEquals(actualMessage1, expectedMessage1);
+
+        WebElement message2 = Driver.getDriver().findElement(By.xpath("(//div[@class='clearfix']//p)[1]"));
+
+        String actualMessage2 = message2.getText();
+        String expectedMessage2 = "Use pin icon on the right top corner of page to create fast access link in the pinbar.";
+
+        Assert.assertEquals(actualMessage2, expectedMessage2);
+
+
+
+    }
+}
